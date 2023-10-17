a0bd89cf3c38bdb54250c234a42b745551dea397
RolandRoman <rrt1709@gmail.com>
2023-10-17 20:52:07 +0700

    push

--- app/src/main/java/com/example/composeUTS/model/DataMatkul.kt
+++ app/src/main/java/com/example/composeUTS/model/DataMatkul.kt
@@ -5,54 +5,54 @@ import com.example.composeUTS.R
 object MatkulsRepository {
     val matkuls = listOf(
         Matkul(
-            matkulNameRes = R.string.matkul1,
-            sksRes = R.string.sks1,
-            imageRes = R.drawable.foto1
+            matkulNameRes = R.string.MataKuliah1,
+            sksRes = R.string.SKS1,
+            imageRes = R.drawable.gambar1
         ),
         Matkul(
-            matkulNameRes = R.string.matkul2,
-            sksRes = R.string.sks2,
-            imageRes = R.drawable.foto2
+            matkulNameRes = R.string.MataKuliah2,
+            sksRes = R.string.SKS2,
+            imageRes = R.drawable.gambar2
         ),
         Matkul(
-            matkulNameRes = R.string.matkul3,
-            sksRes = R.string.sks3,
-            imageRes = R.drawable.foto3
+            matkulNameRes = R.string.MataKuliah3,
+            sksRes = R.string.SKS3,
+            imageRes = R.drawable.gambar3
         ),
         Matkul(
-            matkulNameRes = R.string.matkul4,
-            sksRes = R.string.sks4,
-            imageRes = R.drawable.foto4
+            matkulNameRes = R.string.MataKuliah4,
+            sksRes = R.string.SKS4,
+            imageRes = R.drawable.gambar4
         ),
         Matkul(
-            matkulNameRes = R.string.matkul5,
-            sksRes = R.string.sks5,
-            imageRes = R.drawable.foto5
+            matkulNameRes = R.string.MataKuliah5,
+            sksRes = R.string.SKS5,
+            imageRes = R.drawable.gambar5
         ),
         Matkul(
-            matkulNameRes = R.string.matkul6,
-            sksRes = R.string.sks6,
-            imageRes = R.drawable.foto6
+            matkulNameRes = R.string.MataKuliah6,
+            sksRes = R.string.SKS6,
+            imageRes = R.drawable.gambar6
         ),
         Matkul(
-            matkulNameRes = R.string.matkul7,
-            sksRes = R.string.sks7,
-            imageRes = R.drawable.foto7
+            matkulNameRes = R.string.MataKuliah7,
+            sksRes = R.string.SKS7,
+            imageRes = R.drawable.gambar7
         ),
         Matkul(
-            matkulNameRes = R.string.matkul8,
-            sksRes = R.string.sks8,
-            imageRes = R.drawable.foto8
+            matkulNameRes = R.string.MataKuliah8,
+            sksRes = R.string.SKS8,
+            imageRes = R.drawable.gambar8
         ),
         Matkul(
-            matkulNameRes = R.string.matkul9,
-            sksRes = R.string.sks9,
-            imageRes = R.drawable.foto9
+            matkulNameRes = R.string.MataKuliah9,
+            sksRes = R.string.SKS9,
+            imageRes = R.drawable.gambar9
         ),
         Matkul(
-            matkulNameRes = R.string.matkul10,
-            sksRes = R.string.sks10,
-            imageRes = R.drawable.foto10
+            matkulNameRes = R.string.MataKuliah10,
+            sksRes = R.string.SKS10,
+            imageRes = R.drawable.gambar10
         ),
     )
 }
